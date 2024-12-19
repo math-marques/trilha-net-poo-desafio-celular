13408bb21cc9235a1cc8a50ffc5374cc26ce4910
Matheus Marques <146294794+math-marques@users.noreply.github.com>
2024-12-19 15:20:05 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Uso
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia meuNokia = new Nokia("12345", "Nokia 8708", 123456789, 16);
+        meuNokia.Ligar();
+        meuNokia.ReceberLigacao();
+        meuNokia.InstalarAplicativo("WhatsApp");
+    }
+}
