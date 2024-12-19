146fe8a2cd945b04a6e11a40e03276045c725e8e
Matheus Marques <146294794+math-marques@users.noreply.github.com>
2024-12-19 14:37:14 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,8 +1,15 @@
+//aqui nessa área podem ser adicionados outros USING's, para trazer novas funcionalidades para a aplicação.
+
+//esse namespace representa um espaço que armazenará um conjunto de classes com o mesmo contexto.
 namespace DesafioPOO.Models
 {
+
+//criação da classe abstrata Smartphone. É uma classe que não pode ser instanciada, mas serve de modelo, iniciação, referência, definindo um esqueleto comum para outras classes vindouras.
     public abstract class Smartphone
     {
+        // aqui, pode-se utilizar o famoso "props" + Tab. essas são as prorpriedades da classe ==> numero, smartphone.
         public string Numero { get; set; }
+
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero)
